08db67b83a2242207a210d5a9d97ce600a38fc57
Rodrigo Santos <rssantos07@gmail.com>
2023-03-28 19:51:49 -0300

    ajustes no mobile e insercao de demais coisas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
-   
+    const questions = document.querySelectorAll('[data-faq-question]');
 
-    for (let i = 0; i< buttons.length; i++ ){
-        buttons[i].addEventListener('click', function(botao){
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocutaElementosDoHeader();
+        }else {
+         exibeElementosDoHeader();
+        }
+    })
+
+    //seçao de ataçoes, programaçao da abas
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`)
             escondeTodasAbas();
@@ -12,20 +26,42 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+    // seçao faq acordio
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
-function removeBotaoAtivo(){
+function ocutaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode
+    elementoPai.classList.toggle(classe)
+}
+
+
+function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    for (let i = 0; i< buttons.length; i++ ){
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active')
     }
 }
 
 
-function escondeTodasAbas(){
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for (let i=0; i<tabsContainer.length; i++) {
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
 }
+
